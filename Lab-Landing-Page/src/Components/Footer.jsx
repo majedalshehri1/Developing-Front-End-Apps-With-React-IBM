6c4aae6396274b706eca173e30e07185533bdd9e
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-13 19:04:10 +0300

    Add new files components with Done of the Lab

---
+++ Lab-Landing-Page/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <>
+ <footer>
+ <p>&copy; Event Planner Organization. All rights reserved.</p>
+ </footer>
+        </>
+    )}
+
+export default Footer
