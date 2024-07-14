0116b24a805b6d23086c8f7d96a453f1f222dd3a
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-15 00:45:12 +0300

    Import useState and add three state

--- todo_list/src/Components/TodoList.jsx
+++ todo_list/src/Components/TodoList.jsx
@@ -1,24 +1,32 @@
-import "./TodoList.css"
-
+import { useState } from "react";
+import "./TodoList.css";
 const TodoList = () => {
+  // represent an array of todo items
+  const [todos, setTodos] = useState([]);
+
+  // Represent the value will enter by user into an input field for adding a new heading for a todo item
+  const [headingInput, setHeadingInput] = useState("");
+
+  // Create an empty string representing the value entered into an input field to add a new list item within a todo item.
+  const [listInput, setListInput] = useState("");
+
+  
+
   return (
     <>
-    <div className="todo-container">
+      <div className="todo-container">
         <h1 className="title">My Todo List</h1>
         <div className="input-container">
           <input
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
           />
           <button className="add-list-button">Add Heading</button>
         </div>
       </div>
-      <div className="todo_main">
-        
-      </div>
+      <div className="todo_main"></div>
     </>
-  )
-}
+  );
+};
 export default TodoList;
