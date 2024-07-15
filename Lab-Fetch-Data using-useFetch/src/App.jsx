691e74583193251fee0efd943385dde8c89152ed
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-16 01:41:38 +0300

    Practice Exercise

--- Lab-Fetch-Data using-useFetch/src/App.jsx
+++ Lab-Fetch-Data using-useFetch/src/App.jsx
@@ -1,10 +1,14 @@
 import FetchData from "./Components/FetchData";
+import FetchYogaData from "./Components/FetchYogaData";
 import "./App.css";
 function App() {
   return (
     <>
       <h1>My Data List</h1>
       <FetchData />
+      <h1>Practice Exercise ( With different API )</h1>
+      <FetchYogaData/>
+
     </>
   );
 }
