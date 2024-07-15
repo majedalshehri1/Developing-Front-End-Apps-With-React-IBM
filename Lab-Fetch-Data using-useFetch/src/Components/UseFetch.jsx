71cde70f426faf991b487bf87ff85125ce26eb81
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-16 00:43:39 +0300

    Import useState and useEffect hooks in UseFetch.jsx

--- Lab-Fetch-Data using-useFetch/src/Components/UseFetch.jsx
+++ Lab-Fetch-Data using-useFetch/src/Components/UseFetch.jsx
@@ -1,6 +1,13 @@
+import { useState, useEffect } from "react";
 const UseFetch = () => {
-  
-    
-}
+  //Declare a state variable 'data' and a function 'setData' to update it
+  const[data,setData]=useState();
+  // Use the useEffect hook to perform a side effect (fetch data)
+  useEffect(()=>{
+    fetch().then((res)=>res.json())
+    .then((data)=>setData(data))
+},[])
+return [data]   
+};
 
-export default UseFetch
+export default UseFetch;
