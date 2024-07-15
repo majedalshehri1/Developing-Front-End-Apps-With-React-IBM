67df533e88c0472c8d66ba320b363d5195456f10
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-16 01:00:59 +0300

    To show data in console.log

--- Lab-Fetch-Data using-useFetch/src/Components/UseFetch.jsx
+++ Lab-Fetch-Data using-useFetch/src/Components/UseFetch.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-const UseFetch = () => {
+const UseFetch = (url) => {
   //Declare a state variable 'data' and a function 'setData' to update it
   const[data,setData]=useState();
   // Use the useEffect hook to perform a side effect (fetch data)
   useEffect(()=>{
-    fetch().then((res)=>res.json())
+    fetch(url).then((res)=>res.json())
     .then((data)=>setData(data))
 },[])
 return [data]   
