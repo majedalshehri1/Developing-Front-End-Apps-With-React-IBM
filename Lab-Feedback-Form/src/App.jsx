df26596ba2931dd82147cffd074e71718fcf8b7c
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-16 18:41:09 +0300

    Add react files to third lab

---
+++ Lab-Feedback-Form/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FeedbackForm from "./Components/FeedbackForm";
+
+function App() {
+  return (
+    <>
+      <FeedbackForm />
+    </>
+  );
+}
+
+export default App;
---
+++ Lab-Feedback-Form/src/Components/FeedbackForm.jsx
@@ -0,0 +1,18 @@
+import "./FeedbackForm.css"; // Import CSS for styling
+
+const FeedbackForm = () => {
+  return (
+  <>
+  <nav>
+    Tell Us What You Think
+    </nav>
+      <form className="feedback-form">
+        <h2>Wed Love to Hear From You!</h2>
+        <p>Please share your feedback with us.</p>
+        
+      </form>
+  
+  </>
+  )
+};
+export default FeedbackForm;
