42c74ddc0ededdc2fd0a6123a4aa08f06996fda9
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-16 18:47:01 +0300

    Create basic template in FeedBackForm.jsx

--- Lab-Feedback-Form/src/Components/FeedbackForm.jsx
+++ Lab-Feedback-Form/src/Components/FeedbackForm.jsx
@@ -10,6 +10,22 @@ const FeedbackForm = () => {
         <h2>Wed Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         
+        {/* input form */}
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+        />
+        <textarea
+          name="feedback"
+          placeholder="Your Feedback"
+        ></textarea>
+        <button type="submit">Submit Feedback</button>
       </form>
   
   </>
