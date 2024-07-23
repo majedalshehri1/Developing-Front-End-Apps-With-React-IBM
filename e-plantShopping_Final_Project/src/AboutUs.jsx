9b8537dcb280ee9142d773e82104455d6cfc92ea
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-07-23 05:09:27 +0300

    New Update File

---
+++ e-plantShopping_Final_Project/src/AboutUs.jsx
@@ -0,0 +1,23 @@
+import "./AboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      {/* <h1 className="about-us-heading">About Us</h1> */}
+      <p className="about-us-description">
+        Welcome to Paradise Nursery, where green meets serenity!
+      </p>
+      <span className="about-us-content">
+      Our team of experts is dedicated to ensuring that each plant meets ourstrict standards of quality and care. Whether you re a seasoned gardeneror just starting your green journey, we are here to support you everystep of the way. Feel free to explore our collection, ask questions, andlet us help you find the perfect plant for your home or office.
+        At Paradise Nursery, we are passionate about bringing nature closer toyou. Our mission is to provide a wide range of high-quality plants thatnot only enhance the beauty of your surroundings but also contribute toa healthier and more sustainable lifestyle. From air-purifying plants toaromatic fragrant ones, we have something for every plant enthusiast.
+        Our team of experts is dedicated to ensuring that each plant meets ourstrict standards of quality and care. Whether you re a seasoned gardeneror just starting your green journey, we are here to support you everystep of the way. Feel free to explore our collection, ask questions, andlet us help you find the perfect plant for your home or office.
+        Join us in our mission to create a greener, healthier world. VisitParadise Nursery today and experience the beauty of nature right at yourdoorstep.
+
+      </span>
+      
+      
+    </div>
+  );
+}
+
+export default AboutUs;
